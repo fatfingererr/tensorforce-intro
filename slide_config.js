005cb81e2acd8ff0119ddb78a951a857d0a79579
ffe <fatfingererr@gmail.com>
2018-01-31 22:43:02 +0800

    忘記 Stage Σ(lliдﾟﾉ)ﾉ

--- slide_config.js
+++ slide_config.js
@@ -7,7 +7,7 @@ var SLIDE_CONFIG = {
      usePrettify: true, // Default: true 
      enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
      enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-     favIcon: 'images/emacs-icon.png', 
+     favIcon: 'images/sukki-icon.png', 
      fonts: [
        'Open Sans:regular,semibold,italic,italicsemibold',
        'Source Code Pro'
@@ -16,7 +16,7 @@ var SLIDE_CONFIG = {
  
    // Author information
    presenters: [{
-     name: 'fatfingererr @ Sukki 二月聚會', 
+     name: 'fatfingererr @ Sukki 2018 二月聚會', 
      company: '', 
      gplus: 'https://plus.google.com/fatfingererr', 
      twitter: 'fatfingererr', 
